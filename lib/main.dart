42c97d2bb70cc9cdb0aaa602839b6325a1607ccf
yazumiko <6631501193@lamduan.mfu.ac.th>
2025-09-19 14:45:23 +0700

    add button demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_/week06/baking_app.dart';
+import 'package:flutter_application_/week06/button_demo.dart';
+
 
 
 void main() {
   runApp(
-    MaterialApp(home: BakingApp()));
+    MaterialApp(home: ButtonDemo()));
     }
