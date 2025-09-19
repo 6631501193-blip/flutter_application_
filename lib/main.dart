e7b13e2d776ffec944246d489fcb898bf06aa07d
yazumiko <6631501193@lamduan.mfu.ac.th>
2025-09-19 15:08:07 +0700

    add image demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_/week06/button_demo.dart';
+import 'package:flutter_application_/week06/image_demo.dart';
 
 
 
 void main() {
   runApp(
-    MaterialApp(home: ButtonDemo()));
+    MaterialApp(home: ImageDemo()));
     }
