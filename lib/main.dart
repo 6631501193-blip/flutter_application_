5a7ea529812eb505ff58290f12c9c87ad87835ed
yazumiko <6631501193@lamduan.mfu.ac.th>
2025-09-25 13:59:21 +0700

    add dynamic lopgin design

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_/week06/image_demo.dart';
+import 'package:flutter_application_/week06/login_dynamic.dart';
+
 
 
 
 void main() {
   runApp(
-    MaterialApp(home: ImageDemo()));
+    MaterialApp(home: LoginDynamic()));
     }
