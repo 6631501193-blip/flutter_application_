b60f9bb45007fb8b626791cc7c053166164ee4c7
yazumiko <6631501193@lamduan.mfu.ac.th>
2025-10-17 10:57:49 +0700

    another way

--- lib/week08/page1.dart
+++ lib/week08/page1.dart
@@ -15,14 +15,8 @@ class Page1 extends StatelessWidget {
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => Page2(),
-                settings: RouteSettings(
-                  arguments: <String, dynamic>{
-                    'name': 'apple',
-                    'price': 15,
-                    'date': ['15 Oct', '20 Oct'],
-                  },
-                ),
+                builder: (context) =>
+                    Page2(name: 'Apple', price: 30, date: ['15 Oct', '20 Oct']),
               ),
             );
           },
--- lib/week08/page2.dart
+++ lib/week08/page2.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
 
 class Page2 extends StatelessWidget {
-  const Page2({super.key});
+  String name = '';
+  int price = 0;
+  List date = [];
+
+  Page2({
+    super.key,
+    required this.name,
+    required this.price,
+    required this.date,
+  });
 
   @override
   Widget build(BuildContext context) {
     //get data from previous page
-    Map<String, dynamic> data = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+    // Map<String, dynamic> data =
+    //     ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
 
     return Scaffold(
       appBar: AppBar(title: Text('Page 2')),
       body: Column(
         children: [
-          Text('Name: ${data['name']}'),
-          Text('Price: ${data['price']}'),
-          Text('Expiration Date: ${data['date'][0]}'),
+          Text('Name: $name'),
+          Text('Price: $price'),
+          Text('Expiration Date: $date'),
           FilledButton(
             onPressed: () {
               Navigator.pop(context);
